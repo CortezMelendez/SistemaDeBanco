5b6f5faf9780c892643bdb4ee780f5c093f90b9b
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-26 16:40:52 -0600

    Cree lel metodo para ingresar al usuario y abrir la nueva ventana

--- src/main/java/com/example/welcome/HelloController.java
+++ src/main/java/com/example/welcome/HelloController.java
@@ -1,11 +1,40 @@
 package com.example.welcome;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.print.MultiDocPrintService;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label usuario;
+    @FXML
+    private Label contrasenia;
+    @FXML
+    private TextField intUsuario;
+    @FXML
+    private TextField intContrasenia;
 
+    @FXML
+    protected void abrirVentana(){
+        if (("admin".equals(intUsuario.getText()))&&("admin123".equals(intContrasenia.getText()))) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("SegundaVista2.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setTitle("SegundaVista2");
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.example.welcome {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires javafx.graphics;
 
     opens com.example.welcome to javafx.fxml;
     exports com.example.welcome;
