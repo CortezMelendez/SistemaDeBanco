3bff0cc84e3438929399bb106843a878c3c5d7d8
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-27 16:10:55 -0600

    Modifique los metodos agregando FXML

--- src/main/java/com/example/welcome/Bienvenido.java
+++ src/main/java/com/example/welcome/Bienvenido.java
@@ -1,17 +1,21 @@
 package com.example.welcome;
 
+import javafx.fxml.FXML;
+
 public class Bienvenido {
+    @FXML
     protected void abrirSaldo(){
 
     }
-
+    @FXML
     protected void depositarSaldo(){
 
     }
-
+    @FXML
     protected void retirarSaldo(){
 
     }
+    @FXML
     protected void salir(){
 
     }
