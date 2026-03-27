e2fc2f9cb27e758780dac3e68184b59944ab430a
BrianGalvezchess <20253rd056@utez.edu.mx>
2026-03-27 16:17:26 -0600

    Se hace el programa para consultar saldo desde el metodo

--- src/main/java/com/example/welcome/HelloController2.java
+++ src/main/java/com/example/welcome/HelloController2.java
@@ -1,4 +1,20 @@
 package com.example.welcome;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class HelloController2 {
+
+    @FXML
+    private Label resultado;
+
+    @FXML
+    protected void saldo(){
+
+
+    }
 }
