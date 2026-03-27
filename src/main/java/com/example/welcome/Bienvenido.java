14b89eb21a7a33dd10266306ab52fa9a56b0400d
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-27 15:58:43 -0600

    Cree los metodos para los botones

--- src/main/java/com/example/welcome/Bienvenido.java
+++ src/main/java/com/example/welcome/Bienvenido.java
@@ -1,5 +1,18 @@
 package com.example.welcome;
 
 public class Bienvenido {
+    protected void abrirSaldo(){
 
+    }
+
+    protected void depositarSaldo(){
+
+    }
+
+    protected void retirarSaldo(){
+
+    }
+    protected void salir(){
+
+    }
 }
