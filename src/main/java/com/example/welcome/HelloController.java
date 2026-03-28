9f2a746969cefa8b89f4e5e7d903f48b31982c40
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-28 13:17:51 -0600

    Modifique la clase HelloController haciendola extends de la ClasePadreControladores y agregue un boton para salir completamente del sistema

--- src/main/java/com/example/welcome/HelloController.java
+++ src/main/java/com/example/welcome/HelloController.java
@@ -1,40 +1,36 @@
 package com.example.welcome;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-import javax.print.MultiDocPrintService;
-import java.io.IOException;
 
-public class HelloController {
-    @FXML
-    private Label usuario;
-    @FXML
-    private Label contrasenia;
+public class HelloController extends ClasePadreControladores {
     @FXML
     private TextField intUsuario;
     @FXML
     private TextField intContrasenia;
+    @FXML
+    private Label error;
 
     @FXML
-    protected void abrirVentana(){
-        if (("admin".equals(intUsuario.getText()))&&("admin123".equals(intContrasenia.getText()))) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("BienvenidoUsuario.fxml"));
-                Parent root = loader.load();
-                Stage stage = new Stage();
-                stage.setTitle("SegundaVista2");
-                stage.setScene(new Scene(root));
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    protected void abrirVentana(ActionEvent event){
+
+        if (("admin".equals(intUsuario.getText())) &&
+                ("admin123".equals(intContrasenia.getText()))) {
+
+            cambiarVentana(event, "BienvenidoUsuario.fxml");
+
+        } else {
+            error.setVisible(true);
         }
+
+        }
+    @FXML
+    protected void salir(ActionEvent event) {
+        System.exit(0);
+    }
     }
 
-}
+
