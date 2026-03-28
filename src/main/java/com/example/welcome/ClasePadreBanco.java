6b13d01d4de1a090225bc86f4a3ac30721e45e5a
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-28 13:40:47 -0600

    Cree la clase padre banco y estableci la variable saldo iniciandola en 0 ademas de crear los metodos retirar, depositar y mostrar el saldo

--- src/main/java/com/example/welcome/ClasePadreBanco.java
+++ src/main/java/com/example/welcome/ClasePadreBanco.java
@@ -1,4 +1,26 @@
 package com.example.welcome;
 
 public class ClasePadreBanco {
+ private static double saldo =0;
+
+ public static double getSaldo(){
+     return saldo;
+ }
+    public static boolean depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo = saldo + cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean retirar(double retiro) {
+        if (retiro>0 && retiro <=saldo) {
+            saldo = saldo-retiro;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
