2deabab5a6fc5731fd35ac4c535f49c3b23187f9
CortezMelendez <20253rd043@utez.edu.mx>
2026-03-28 22:06:03 -0600

    Agregue informacion a los metodos de abrir, reitrar, y salir

--- src/main/java/com/example/welcome/Bienvenido.java
+++ src/main/java/com/example/welcome/Bienvenido.java
@@ -1,22 +1,31 @@
 package com.example.welcome;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class Bienvenido {
+public class Bienvenido extends ClasePadreControladores {
     @FXML
-    protected void abrirSaldo(){
+    protected void abrirSaldo(ActionEvent event) {
 
     }
+
     @FXML
-    protected void depositarSaldo(){
+    protected void depositarSaldo(ActionEvent event) {
+        cambiarVentana(event,"depositar-view.fxml");
+    }
+
+    @FXML
+    protected void retirarSaldo(ActionEvent event) {
+        cambiarVentana(event,"Saldo.fxml");
 
     }
-    @FXML
-    protected void retirarSaldo(){
 
-    }
     @FXML
-    protected void salir(){
-
+    protected Bienvenido salir(ActionEvent event) {
+        cambiarVentana(event, "hello-view.fxml");
     }
+
+
 }
+
+
