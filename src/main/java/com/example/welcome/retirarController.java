b9d3ceaccd97fe573d4594af5f607cdf0cb62b6a
angel <>
2026-03-29 17:12:50 -0600

    Creacion de archivos para retiro

---
+++ src/main/java/com/example/welcome/retirarController.java
@@ -0,0 +1,23 @@
+package com.example.welcome;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class retirarController extends ClasePadreControladores{
+
+    @FXML
+    private TextField getRetiro;
+    @FXML
+    private Label saldo;
+    @FXML
+    private Label logs;
+
+}
