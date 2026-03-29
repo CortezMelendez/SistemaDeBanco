5e7f546fe4f25f9480eb7ab54ab68d37134c462f
BrianGalvezchess <20253rd056@utez.edu.mx>
2026-03-28 22:00:33 -0600

    reemplazo el metodo regresar por salir usando cambiarVentana

--- src/main/java/com/example/welcome/HelloController2.java
+++ src/main/java/com/example/welcome/HelloController2.java
@@ -1,13 +1,11 @@
 package com.example.welcome;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-public class HelloController2 extends  ClasePadreBanco {
+
+public class HelloController2 extends  ClasePadreControladores {
 
     @FXML
     private Label resultado;
@@ -18,18 +16,8 @@ public class HelloController2 extends  ClasePadreBanco {
     }
 
     @FXML
-    protected void regresar(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("BienvenidoUsuario.fxml"));
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Menu");
-            stage.setScene(new Scene(root));
-            stage.show();
-            Stage stageActual = (Stage) resultado.getScene().getWindow();
-            stageActual.close();
-        } catch (Exception e) {
-            resultado.setText("Error al regresar");
-        }
+    protected void salir(ActionEvent event) {
+        cambiarVentana(event, "BienvenidoUsuario.fxml");
     }
+
 }
